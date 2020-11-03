83b9acd9a6894ba4a1fe249dbbc9532a1a6b1e2b
unsivil <unsivilaudio@gmail.com>
2020-11-03 14:32:45 -0700

    implement first Ts controller

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import './controllers/LoginController';
 
+import ControllerRouter from './controllers/decorators/controller';
 import LoginRoutes from './routes/loginRoutes';
 
 const app = express();
@@ -9,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['fdafwfdf'] }));
 
+app.use(ControllerRouter);
 app.use(LoginRoutes);
 
 app.listen(port, () => {
